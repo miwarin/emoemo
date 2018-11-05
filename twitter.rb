dcd7061e2c42dcd0525a2999e38ca4d58989bc92
Susumu Miwa <miwa@soum.co.jp>
2018-11-05 17:57:09 +0900

    twitter.rb: use mackerel

--- twitter.rb
+++ twitter.rb
@@ -4,25 +4,92 @@
 
 require 'twitter'
 require 'natto'
+require 'net/https'
+require 'uri'
+require 'json'
+require "sqlite3"
 require 'pp'
 
 module Emoemo
+  class Service
+    def initialize()
+      @uri = URI('https://api.mackerelio.com/api/v0/services/emotion/tsdb')
+    end
+
+    def post(tweets, scores)
+      request = Net::HTTP::Post.new(@uri)
+      request['Content-Type'] = "application/json"
+      request['X-Api-Key'] = 'xxxx'
+
+      tweets.each_with_index {|tweet, index|
+        documents = [{
+            'name' => 'emotion',
+            'time' => DateTime.parse(tweet["created_at"]).strftime('%s').to_i,
+            'value' => scores[index]
+        }]
+        request.body = documents.to_json
+  
+        response = Net::HTTP.start(@uri.host, @uri.port, :use_ssl => @uri.scheme == 'https') do |http|
+            http.request (request)
+        end
+      }
+    end
+  end
+
+  class TextAnalyzer
+    def initialize()
+      @accessKey = 'xxxx'
+      uri = 'https://japaneast.api.cognitive.microsoft.com'
+      path = '/text/analytics/v2.0/sentiment'
+      @uri = URI(uri + path)
+    end
+
+    def analyze(tweets)
+      docs ||= []
+      id = 1
+      tweets.each {|t|
+        docs << { 'id' => id.to_s, 'language' => 'ja', 'text' => t["text"] }
+        id = id + 1
+      }
+
+      documents = { 'documents': docs }
+      
+      request = Net::HTTP::Post.new(@uri)
+      request['Content-Type'] = "application/json"
+      request['Ocp-Apim-Subscription-Key'] = @accessKey
+      request.body = documents.to_json
+      
+      response = Net::HTTP.start(@uri.host, @uri.port, :use_ssl => @uri.scheme == 'https') do |http|
+          http.request (request)
+      end
+      
+      scores ||= []
+      json = JSON.parse(response.body)
+      json["documents"].each {|doc|
+        scores << doc["score"]
+      }
+      pp scores
+      return scores
+    end
+  end
 
   class EmoTwitter
     def initialize()
+      @cache_path = "./cache.db"
+
       proxy = {
-        host: "proxy.example.jp",
+        host: "proxy2.example.co.jp",
         port: 8080,
-        username: "user",
-        password: "password"
+        username: "xxxx",
+        password: "xxxx"
       }
 
       # ログイン
       @client = Twitter::REST::Client.new do |config|
         config.consumer_key = 'xxxx'
         config.consumer_secret = 'xxxx'
-        config.access_token = 'xxxx'
         config.access_token_secret = 'xxxx'
+        config.access_token = 'xxxx'
         config.proxy = proxy
       end 
     end
@@ -34,23 +101,70 @@ module Emoemo
       puts @client.user.tweets_count  # ツイート数
     end
 
-    # 1件だけ取得
     def get_tweet(username)
-      @client.user_timeline(username, count:1).each {|tweet|
+      t ||= []
+      @client.user_timeline(username).each {|tweet|
+        puts tweet.created_at
         puts tweet.text
         puts tweet.user.screen_name
         puts tweet.id
+        t << {"id" => tweet.id, "created_at" => tweet.created_at.to_s, "text" => tweet.text}
       }
+      return t
     end
+
+    def get_new_tweets(username)
+      db = SQLite3::Database.new(@cache_path)
+      begin
+        rows = db.execute <<-SQL
+          create table tweet (
+            id text,
+            created_at text,
+            text text
+          );
+        SQL
+      rescue => e
+        puts e
+      end
+
+      now_tweets = get_tweet(username)
+      puts "now_tweets:"
+      pp now_tweets
+      puts ""
+
+      new_tweets ||= []
+
+      now_tweets.each {|n|
+        if db.execute( "select * from tweet where id == #{n['id']}" ).length == 0
+          puts "new tweet: #{n}"
+          db.execute "insert into tweet values ( ?, ?, ? )", n["id"], n["created_at"], n["text"]
+          new_tweets << n
+        end
+      }
+      puts "new_tweets:"
+      pp new_tweets
+      puts ""
+
+      return new_tweets
+    end
+
   end # class EmoTwitter
 
   class Emoemo
     def initialize()
       @twitter = EmoTwitter.new()
+      @analyzer = TextAnalyzer.new()
+      @service = Service.new()
     end
 
     def analyze()
-      @twitter.get_tweet("miwarin")
+      tweets = @twitter.get_new_tweets("miwarin")
+      if tweets.length == 0
+        puts "not new tweet"
+        return
+      end
+      scores = @analyzer.analyze(tweets)
+      @service.post(tweets, scores)
     end
   end # class Emoemo
 end # module Emoemo
