909aa05df032c5afd9c19d3de21be3ae4f8d0f53
Susumu Miwa <miwa@soum.co.jp>
2018-11-19 12:57:13 +0900

    twitter.rb: add cache, add show_score, add CONSTANT VALUE

--- twitter.rb
+++ twitter.rb
@@ -8,23 +8,83 @@ require 'net/https'
 require 'uri'
 require 'json'
 require "sqlite3"
+require 'time'
 require 'pp'
 
+MACERELIO_APIKEY = 'xxx'
+
+TEXTANALYZER_APIKEY = 'xxx'
+
+TWITTER_COMSUMER_KEY = 'xxx'
+TWITTER_COMSUMER_SECRET = 'xxx'
+TWITTER_ACCESS_TOKEN_SECRET = 'xxx'
+TWITTER_ACCESS_TOKEN = 'xxx'
+
+PROXY_HOST = 'proxy.example.co.jp'
+PROXY_PORT = 8080
+PROXY_USERNAME = 'xxx'
+PROXY_PASSWORD = 'xxx'
+
+
 module Emoemo
-  class Service
-    def initialize()
+  class ECache
+    def initialize(cache_path)
+      @db = SQLite3::Database.new(cache_path)
+      begin
+        rows = @db.execute <<-SQL
+          create table tweet (
+            id text,
+            created_at text,
+            text text,
+            score text
+          );
+        SQL
+      rescue => e
+        puts e
+      end
+    end
+
+    def find_new_tweets(now_tweets)
+      new_tweets ||= []
+      now_tweets.each {|n|
+        if @db.execute( "select * from tweet where id == #{n['id']}" ).length == 0
+          puts "new tweet: #{n}"
+          @db.execute "insert into tweet values ( ?, ?, ?, ? )", n["id"], n["created_at"], n["text"], "0.0"
+          new_tweets << n
+        end
+      }
+      return new_tweets
+    end
+
+    def update_tweet(id, created_at, text, score)
+      @db.execute("update tweet set created_at='#{created_at}', score=#{score} where id=#{id}")
+    end
+
+    def dump()
+      # レコードを取得する
+      @db.execute( "select * from tweet" ) do |row|
+        p row
+      end
+    end
+
+  end # class ECache
+  
+  class Mackerelio
+    def initialize(cache)
+      @cache = cache
       @uri = URI('https://api.mackerelio.com/api/v0/services/emotion/tsdb')
     end
 
     def post(tweets, scores)
       request = Net::HTTP::Post.new(@uri)
       request['Content-Type'] = "application/json"
-      request['X-Api-Key'] = 'xxxx'
+      request['X-Api-Key'] = MACERELIO_APIKEY
 
       tweets.each_with_index {|tweet, index|
+        epoch_time = Time.parse(tweet["created_at"]).strftime('%s').to_i
         documents = [{
             'name' => 'emotion',
-            'time' => DateTime.parse(tweet["created_at"]).strftime('%s').to_i,
+            'time' => epoch_time,
             'value' => scores[index]
         }]
         request.body = documents.to_json
@@ -32,13 +92,15 @@ module Emoemo
         response = Net::HTTP.start(@uri.host, @uri.port, :use_ssl => @uri.scheme == 'https') do |http|
             http.request (request)
         end
+
+        @cache.update_tweet(tweet["id"], Time.at(epoch_time), tweet["text"], scores[index].to_s)
       }
     end
-  end
+  end # Mackerelio
 
   class TextAnalyzer
     def initialize()
-      @accessKey = 'xxxx'
+      @accessKey = TEXTANALYZER_APIKEY
       uri = 'https://japaneast.api.cognitive.microsoft.com'
       path = '/text/analytics/v2.0/sentiment'
       @uri = URI(uri + path)
@@ -71,25 +133,25 @@ module Emoemo
       pp scores
       return scores
     end
-  end
+  end # class TextAnalyzer
 
   class EmoTwitter
-    def initialize()
-      @cache_path = "./cache.db"
+    def initialize(cache)
+      @cache = cache
 
       proxy = {
-        host: "proxy2.example.co.jp",
-        port: 8080,
-        username: "xxxx",
-        password: "xxxx"
+        host: PROXY_HOST,
+        port: PROXY_PORT,
+        username: PROXY_USERNAME,
+        password: PROXY_PASSWORD
       }
 
       # ログイン
       @client = Twitter::REST::Client.new do |config|
-        config.consumer_key = 'xxxx'
-        config.consumer_secret = 'xxxx'
-        config.access_token_secret = 'xxxx'
-        config.access_token = 'xxxx'
+        config.consumer_key = TWITTER_COMSUMER_KEY
+        config.consumer_secret = TWITTER_COMSUMER_SECRET
+        config.access_token_secret = TWITTER_ACCESS_TOKEN_SECRET
+        config.access_token = TWITTER_ACCESS_TOKEN
         config.proxy = proxy
       end 
     end
@@ -114,33 +176,12 @@ module Emoemo
     end
 
     def get_new_tweets(username)
-      db = SQLite3::Database.new(@cache_path)
-      begin
-        rows = db.execute <<-SQL
-          create table tweet (
-            id text,
-            created_at text,
-            text text
-          );
-        SQL
-      rescue => e
-        puts e
-      end
-
       now_tweets = get_tweet(username)
       puts "now_tweets:"
       pp now_tweets
       puts ""
 
-      new_tweets ||= []
-
-      now_tweets.each {|n|
-        if db.execute( "select * from tweet where id == #{n['id']}" ).length == 0
-          puts "new tweet: #{n}"
-          db.execute "insert into tweet values ( ?, ?, ? )", n["id"], n["created_at"], n["text"]
-          new_tweets << n
-        end
-      }
+      new_tweets = @cache.find_new_tweets(now_tweets)
       puts "new_tweets:"
       pp new_tweets
       puts ""
@@ -152,9 +193,10 @@ module Emoemo
 
   class Emoemo
     def initialize()
-      @twitter = EmoTwitter.new()
+      @cache = ECache.new("./cache.db")
+      @twitter = EmoTwitter.new(@cache)
       @analyzer = TextAnalyzer.new()
-      @service = Service.new()
+      @mackerel = Mackerelio.new(@cache)
     end
 
     def analyze()
@@ -164,14 +206,22 @@ module Emoemo
         return
       end
       scores = @analyzer.analyze(tweets)
-      @service.post(tweets, scores)
+      @mackerel.post(tweets, scores)
+    end
+
+    def show_score()
+      @cache.dump()
     end
   end # class Emoemo
 end # module Emoemo
 
 def main(argv)
   emo = Emoemo::Emoemo.new()
-  emo.analyze()
+  if argv.length == 0
+    emo.analyze()
+  else
+    emo.show_score()
+  end
 end
 
 main(ARGV)
